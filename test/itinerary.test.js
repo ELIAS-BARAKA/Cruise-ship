8c4d1e50252848e2f403544b7903c8ee89c157db
ELIAS-BARAKA <dbarakaelias@outlook.com>
2022-08-29 16:30:33 +0300

    dried the code erfljnlrk exit

--- test/itinerary.test.js
+++ test/itinerary.test.js
@@ -2,21 +2,22 @@ const Itinerary = require("../src/itinerary.js");
 const Port = require("../src/port.js");
 
 describe("Itinerary", () => {
+    let dover;
+    let calais;
+    let itinerary;
+    beforeEach(() => {
+        dover = new Port('dover');
+        calais = new Port("calais");
+    })
     it("verify if the object was created", () => {
         expect(new Itinerary("")).toBeInstanceOf(Object);
     });
     it("verfify if itinerary can have ports", () => {
-        const dover = new Port("dover");
-        const itinerary = new Itinerary([dover]);
-        
+        itinerary = new Itinerary([dover]);
         expect(itinerary.ports).toEqual([dover]);
     })
     it("create an itinerary and verifies it", () => {
-        const dover = new Port("dover");
-        const calais = new Port("calais");
-
-        const itinerary = new Itinerary([dover, calais]);
-
+        itinerary = new Itinerary([dover, calais]);
         expect(itinerary.ports).toEqual([dover, calais]);
     });
 });
